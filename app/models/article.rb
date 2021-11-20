b20cb6f61efb4c4b797c598c660adfc411cb9975
PaleAlex <aless.ciocchetti@gmail.com>
2021-11-20 12:01:55 +0100

    Aggiornamento 20/11 per rimessa in funzione del blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   validates :description, presence: true, length: { minimum: 100, maximum: 5000 }
+
 end
